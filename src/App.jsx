e1e581085c5faa05fd5d8d01ad3258fe734f4387
DJ-BoT07 <djmangaonkar7@gmail.com>
2024-04-18 17:57:03 +0530

    Stacked

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import FooterTail from "./Helper/FooterTail";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen text-4xl font-bold">
+        Welcome Web Developers!
+      </div>
+    </>
+  );
+}
+
+export default App;
